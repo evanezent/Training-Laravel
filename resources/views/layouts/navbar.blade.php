7330e3cad1fd98a742e1018238698bf75fc883cb
Pratama Yoga <pratamayo16@gmail.com>
2019-12-28 00:30:31 +0700

    update frontend

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -52,7 +52,7 @@
                     <!-- Left Side Of Navbar -->
 
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active"><a class="nav-link" href="{{ url('table') }}">Table</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('table') }}">Table</a></li>
                         <li class="nav-item"><a class="nav-link" href="#">APAAN YAK</a></li>
                     </ul>
 
